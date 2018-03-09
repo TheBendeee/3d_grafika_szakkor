a98403291d8b6c411b56e85978ff455bb73004ac
dawars <dawars00@gmail.com>
2018-03-09 10:17:11 +0100

    Added MathUtils

---
+++ src/main/java/me/dawars/utils/MathUtils.java
@@ -0,0 +1,11 @@
+package me.dawars.utils;
+
+import org.joml.Matrix4f;
+import processing.core.PMatrix3D;
+
+public class MathUtils {
+
+    public static PMatrix3D toPMatrix(Matrix4f mat){
+        return null;
+    }
+}
